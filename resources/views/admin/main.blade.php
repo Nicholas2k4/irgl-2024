3b451284be449e6e32fd30e5354017a2e92e6cb3
c14230088 <Tanaka272cool@gmail.com>
2024-06-09 15:02:01 +0700

    initial

---
+++ resources/views/admin/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN PAGE | IRGL 2024</title>
+
+    {{-- Swal --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+
+    <!-- <link rel="icon" type="image/x-icon" href="{{ asset('storage/assets/img/logo.png') }}"> --> <!-- GANTI ICON IRGL, cari tempat-->
+    <link rel="canonical" href="https://irgl.petra.ac.id/">
+
+    <!-- AJAX -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+<body>
+    <nav class="">
+
+    </nav>
+</body>
+</html>

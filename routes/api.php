c58f130a5b76c79a4462bfc7f2b75e19cea5dbcc
Girpongg <c14220167@john.petra.ac.id>
2024-07-13 00:11:49 +0700

    Feat: Terbaruh

--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,17 @@ Route::get('/coba', function () {
     return response()->json(['message' => 'Hello, World!']);
 });
 
-
-
-
 Route::post('login', [AuthController::class, 'login']);
-
-Route::post('getUserData',[TeamController::class, 'getUserData'])->middleware('auth:sanctum');
-Route::post('getUserDataByGamePass',[TeamController::class, 'getUserDataByGamePass']);
-Route::post('uploadScoreWin',[TeamController::class, 'uploadScoreWin'])->middleware('auth:sanctum');
-Route::post('uploadScoreLose',[TeamController::class, 'uploadScoreLose'])->middleware('auth:sanctum');
-Route::post('getUserStreak',[TeamController::class, 'getUserStreak']);
-Route::post('getUserGrandPrizeStreak',[TeamController::class, 'getUserGrandPrizeStreak']);
-Route::post('updateResultOfRoulette',[TeamController::class, 'updateResultOfRoulette']);
-Route::post('getGameLinkById',[TeamController::class, 'getGameLinkById']);
-Route::post('getGameByRoulette',[TeamController::class, 'getGameByRoulette']);
-
-Route::post('getQuestion',[TeamController::class, 'getQuestion']);
-Route::post('uploadQuestion',[TeamController::class, 'uploadQuestion']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('getUserData', [TeamController::class, 'getUserData']);
+    Route::post('getUserDataByGamePass', [TeamController::class, 'getUserDataByGamePass']);
+    Route::post('uploadScoreWin', [TeamController::class, 'uploadScoreWin']);
+    Route::post('uploadScoreLose', [TeamController::class, 'uploadScoreLose']);
+    Route::post('getUserStreak', [TeamController::class, 'getUserStreak']);
+    Route::post('getUserGrandPrizeStreak', [TeamController::class, 'getUserGrandPrizeStreak']);
+    Route::post('updateResultOfRoulette', [TeamController::class, 'updateResultOfRoulette']);
+    Route::post('getGameLinkById', [TeamController::class, 'getGameLinkById']);
+    Route::post('getGameByRoulette', [TeamController::class, 'getGameByRoulette']);
+    Route::post('getQuestion', [TeamController::class, 'getQuestion']);
+    Route::post('uploadQuestion', [TeamController::class, 'uploadQuestion']);
+});

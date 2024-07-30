bfe330175cee34601c1c4e23e08927cc51e5d716
c14230088 <141811484+c14230088@users.noreply.github.com>
2024-07-30 21:29:36 +0700

    Update rekapTeam.blade.php

--- resources/views/admin/rekapTeam.blade.php
+++ resources/views/admin/rekapTeam.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 <div class="justify-center items-center flex max-w-[100vw] min-h-screen">
-    <div class="w-[96.6vw] justify-evenly items-center flex flex-col rounded-2xl bg-white">
+    <div class="w-[93.6vw] justify-evenly items-center flex flex-col rounded-2xl bg-white">
         
         <!-- <div class="w-full h-[15%] flex items-center justify-center mt-6">
             <label for="inputCari">Search:</label>

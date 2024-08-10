784688b818539a7290100cd308d67d6dc1df68df
ChristopherJ123 <christopherz2002@hotmail.com>
2024-08-11 01:20:59 +0700

    small tweak to form horizontal size

--- resources/views/registration/registration-step-one.blade.php
+++ resources/views/registration/registration-step-one.blade.php
@@ -1,7 +1,7 @@
 @extends('register')
 
 @section('container')
-<div class="bg-[rgba(0,0,0,0.5)] backdrop-blur-lg shadow-[0_0_20px_rgba(255,255,255,0.1)] md:w-fit md:min-w-[400px] w-5/6 p-10 rounded-[20px] text-center m-8 relative">
+<div class="bg-[rgba(0,0,0,0.5)] backdrop-blur-lg shadow-[0_0_20px_rgba(255,255,255,0.1)] md:w-fit md:max-w-[420px] w-5/6 p-10 rounded-[20px] text-center m-8 relative">
     <h2 class="text-xl md:text-2xl text-white font-bold mb-4">Register</h2>
     <form action="{{ route('register.show.step.one') }}" method="POST" enctype="multipart/form-data">
         @csrf
@@ -65,7 +65,7 @@
                 <input type="file" id="file-ketua-id" name="fileKetua" accept="image/*" class="absolute inset-0 opacity-0 w-full h-full cursor-pointer" onchange="showFileName('ketua')">
                 <span class="text-2xl material-icons">cloud_upload</span>
             </div>
-            <div id="img-name-ketua" class="text-sm md:text-base text-white mt-2 text-left break-words max-w-[300px]">
+            <div id="img-name-ketua" class="text-sm md:text-base text-white mt-2 text-left break-words">
                 @if (session('step1.fileKetua'))
                     {{ basename(session('step1.fileKetua')) }}
                 @endif

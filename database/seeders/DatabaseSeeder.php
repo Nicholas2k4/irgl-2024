affa068238fae104fa1d47996ddc68155feef97e
ChristopherJ123 <christopherz2002@hotmail.com>
2024-09-01 11:59:23 +0700

    enable team seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(JadwalSeeder::class);
         // $this->call(GameSeeder::class);
 
-        // $this->call(TeamSeeder::class);
+        $this->call(TeamSeeder::class);
         // $this->call(StatisticTeamSeeder::class);
         // $this->call(ElimQuestionSeeder::class);
     }

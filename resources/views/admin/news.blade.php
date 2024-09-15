2e5a85d01eac34658e83f38eeb5add2727864085
Nicholas2k4 <nicholas.s2k4@gmail.com>
2024-09-15 14:28:10 +0700

    chore: news info styling

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -31,18 +31,18 @@
 
     <div class="h-fit p-10 text-center">
         <div class="bg-gradient-to-r from-[#133D7D] to-[#711191]  rounded-lg px-7 py-4 w-full md:w-1/2 mx-auto">
-            <h1 class="text-left text-white text-xl">Slug: {{ $current_news->slug }}</h1>
-            <h1 class="text-left text-white text-xl">Content: {{ $current_news->content }}</h1>
-            <h1 class="text-left text-white text-xl">Email filter price: {{ $current_news->email_filter_price }}</h1>
-            <h1 class="text-left text-white text-xl">Encryption machine price: {{ $current_news->encryption_machine_price }}
+            <h1 class="text-left text-white text-3xl font-bold mb-1">{{ $current_news->slug }}</h1>
+            <h1 class="text-left text-white text-md">Content: {{ $current_news->content }}</h1>
+            <h1 class="text-left text-white text-md">Email filter price: {{ $current_news->email_filter_price }}</h1>
+            <h1 class="text-left text-white text-md">Encryption machine price: {{ $current_news->encryption_machine_price }}
             </h1>
-            <h1 class="text-left text-white text-xl">Traffic controller price: {{ $current_news->traffic_controller_price }}
+            <h1 class="text-left text-white text-md">Traffic controller price: {{ $current_news->traffic_controller_price }}
             </h1>
-            <h1 class="text-left text-white text-xl">Antivirus price: {{ $current_news->antivirus_price }}</h1>
-            <h1 class="text-left text-white text-xl">Input validator price: {{ $current_news->input_validator_price }}</h1>
-            <h1 class="text-left text-white text-xl">Slot left: {{ $current_news->slot }}</h1>
-            <h1 class="text-left text-white text-xl">Next news slug: {{ $current_news->next_slug }}</h1>
-            <h1 class="text-left text-white text-xl">Attack type: {{ $current_news->attack_type }}</h1>
+            <h1 class="text-left text-white text-md">Antivirus price: {{ $current_news->antivirus_price }}</h1>
+            <h1 class="text-left text-white text-md">Input validator price: {{ $current_news->input_validator_price }}</h1>
+            <h1 class="text-left text-white text-xl font-bold mt-3 mb-1">Slot left: {{ $current_news->slot }}</h1>
+            <h1 class="text-left text-white text-md">Next news slug: {{ $current_news->next_slug }}</h1>
+            <h1 class="text-left text-white text-xl font-bold mt-3">Attack type: {{ $current_news->attack_type }}</h1>
 
             <form action="{{ route('admin.news.skipNews') }}" method="POST" id="form-next">
                 @csrf

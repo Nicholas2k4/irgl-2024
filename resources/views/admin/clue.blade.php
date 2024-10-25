2ff220ce20ef5d9df5c3bfcea7ebb113d383e1c7
Nicholas2k4 <nicholas.s2k4@gmail.com>
2024-10-26 00:52:49 +0700

    feat: clue shop completed

--- resources/views/admin/clue.blade.php
+++ resources/views/admin/clue.blade.php
@@ -46,29 +46,32 @@
                         </ul>
                     </div>
                 </div>
+
                 <div class="relative mb-5">
                     <h2 class="text-white text-start">Clue</h2>
-                    <h1 class="text-2xl mb-3 text-amber-300 font-bold text-start" id='price-field'>price: -</h1>
-                    <input type="text" placeholder="Search clue..."
-                        class="w-full rounded-lg bg-transparent text-white border-white focus:border-indigo-500 focus:ring-indigo-500"
-                        autocomplete="off" name="clue" id="clue-placeholder">
-                    <div class="absolute z-10 w-full mt-1 bg-gray-800 rounded-lg shadow-lg hidden" id="dropdown-clue">
-                        <ul class="max-h-60 overflow-y-auto">
-                            <li class="p-2 text-white cursor-pointer hover:bg-indigo-500">Clue 1</li>
-                            <li class="p-2 text-white cursor-pointer hover:bg-indigo-500">Clue 1</li>
-                            <li class="p-2 text-white cursor-pointer hover:bg-indigo-500">Clue 1</li>
-                            <li class="p-2 text-white cursor-pointer hover:bg-indigo-500">Clue 1</li>
-                            <li class="p-2 text-white cursor-pointer hover:bg-indigo-500">Clue 1</li>
-                        </ul>
-                    </div>
+                    <h1 class="text-2xl mb-3 text-amber-300 font-bold text-start" id="price-field">price: -</h1>
+                    <select
+                        class="w-full rounded-lg bg-transparent text-white border border-white focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none font-bold"
+                        name="clue" id="clue-dropdown">
+                        <option value="" disabled selected class="bg-gray-800 text-white">Select a clue...</option>
+                        <option value="1" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-1-option'>Clue 1</option>
+                        <option value="2" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-2-option'>Clue 2</option>
+                        <option value="3" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-3-option'>Clue 3</option>
+                        <option value="4" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-4-option'>Clue 4</option>
+                        <option value="5" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-5-option'>Clue 5</option>
+                        <option value="6" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-6-option'>Clue 6</option>
+                        <option value="7" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-7-option'>Dictionary 1</option>
+                        <option value="8" class="bg-gray-800 text-white hover:bg-indigo-500 clue-options"
+                            id='clue-8-option'>Dictonary 2</option>
+                    </select>
                 </div>
-                {{-- <div class="flex flex-col items-start mb-5">
-                    <h2 class="text-white mb-1">Email Filter <i class="text-sm font-bold">{{ '@' . $email_price }}</i></h2>
-                    <input type="number" id="email-qty"
-                        class="item-qty w-full rounded-lg bg-transparent text-white border-white focus:border-indigo-500 focus:ring-indigo-500"
-                        name="email-qty">
-                </div> --}}
-
             </form>
             <button id='submit-buy'
                 class="bg-gradient-to-r from-green-300 to-green-700
@@ -88,14 +91,22 @@
             const input = $('#team-name');
             const dropdown = $('#dropdown');
             const dropdownList = $('#dropdown-list');
-            const dropdownClue = $('#dropdown-clue');
             const teams = @json($teams);
 
-            const cluePlaceholder = $('#clue-placeholder');
+            // clue selections
+            const clue1 = $('#clue-1-option');
+            const clue2 = $('#clue-2-option');
+            const clue3 = $('#clue-3-option');
+            const clue4 = $('#clue-4-option');
+            const clue5 = $('#clue-5-option');
+            const clue6 = $('#clue-6-option');
+            const clue7 = $('#clue-7-option');
+            const clue8 = $('#clue-8-option');
 
+            // live team search
             input.on('input', function() {
                 const query = input.val().toUpperCase();
-                dropdownList.empty(); // Clear previous options
+                dropdownList.empty();
 
                 teams.forEach(team => {
                     if (team.nama.toUpperCase().includes(query)) {
@@ -104,7 +115,8 @@
                             .on('click', function() {
                                 input.val(team.nama.toUpperCase());
                                 $('#score-field').html(team.nama + ' score: ' + team.score);
-                                dropdown.addClass('hidden'); // Hide dropdown on selection
+                                dropdown.addClass('hidden');
+                                updateClueDropdown(team);
                             });
                         dropdownList.append(listItem);
                     }
@@ -112,23 +124,42 @@
                 dropdown.removeClass('hidden');
             });
 
-            $(document).on('click', function(event) {
-                dropdown.addClass('hidden');
-                if (!dropdownClue.is(event.target) && !dropdownClue.has(event.target).length &&
-                    !cluePlaceholder.is(event.target) && !cluePlaceholder.has(event.target).length) {
-                    dropdownClue.addClass('hidden'); 
-                }
+            // make clue selections according to selected team statistics
+            function updateClueDropdown(team) {
+                if (team.has_clue1 == 0) clue1.removeClass('hidden');
+                else clue1.addClass('hidden');
+                if (team.has_clue2 == 0) clue2.removeClass('hidden');
+                else clue2.addClass('hidden');
+                if (team.has_clue3 == 0) clue3.removeClass('hidden');
+                else clue3.addClass('hidden');
+                if (team.has_clue4 == 0) clue4.removeClass('hidden');
+                else clue4.addClass('hidden');
+                if (team.has_clue5 == 0) clue5.removeClass('hidden');
+                else clue5.addClass('hidden');
+                if (team.has_clue6 == 0) clue6.removeClass('hidden');
+                else clue6.addClass('hidden');
+                if (team.has_clue7 == 0) clue7.removeClass('hidden');
+                else clue7.addClass('hidden');
+                if (team.has_clue8 == 0) clue8.removeClass('hidden');
+                else clue8.addClass('hidden');
+            }
+
+            // clue prices
+            $('#clue-dropdown').on('change', function() {
+                let selectedValue = parseInt($(this).val());
+                let priceContent = 'price: ' + (selectedValue < 7 ? '15' : '100');
+                $('#price-field').html(priceContent);
             });
 
-            cluePlaceholder.on('click', function(event) {
-                event.stopPropagation();
-                dropdownClue.removeClass('hidden');
+            // hide team dropdown
+            $(document).on('click', function(event) {
+                dropdown.addClass('hidden');
             });
 
             // Confirmation before submit
             $('#submit-buy').on('click', function(event) {
                 Swal.fire({
-                    title: "Buy clue for team " + $('#team-name').val() + "?",
+                    title: "Buy " + $('#clue-dropdown').find('option:selected').text() + " for team " + $('#team-name').val() + "?",
                     showCancelButton: true,
                     confirmButtonText: "BUY!",
                 }).then((result) => {
